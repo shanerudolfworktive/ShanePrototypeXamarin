d57b80227822608dec0b8709057d076d93ca2fe2
Shane Rudolf <shane1@Shanes-MacBook-Pro.local>
2017-07-07 11:57:08 -0400

    Experiment Markup Extensions

---
+++ ShanePrototypeXamarin/MarkupExtensions/EmbeddedImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+namespace ShanePrototypeXamarin.MarkupExtensions
+{
+    [ContentProperty("ResourceId")]
+    public class EmbeddedImage : IMarkupExtension
+    {
+        public string ResourceId { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (String.IsNullOrWhiteSpace(ResourceId)) return null;
+            return ImageSource.FromResource(ResourceId);
+        }
+    }
+}
